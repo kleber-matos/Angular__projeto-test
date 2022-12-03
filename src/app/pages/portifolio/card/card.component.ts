0fd2af13537afdbf0da089d4c5ee0485a3b3803f
kleber-matos <Klebermatos2019@icloud.com>
2022-12-03 14:53:23 -0300

    commit

--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
+
 export class CardComponent {
+
+  constructor(
+    private id: ActivatedRoute
+
+  ){
+    this.id.params.subscribe(
+      res => console.log(res)
+    )
+  }
+
 
 }
